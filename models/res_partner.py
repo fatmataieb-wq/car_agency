01eba918ff343d915801f0f9a6872f11071f71f8
fatmataieb-wq <ft@softifi.com>
2026-08-12 13:06:05 +0100

    Clean up res_partner.py
    Removed unnecessary comments explaining the extension of res.partner.

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,14 +1,5 @@
-# -*- coding: utf-8 -*-
 from odoo import fields, models
-
-
 class ResPartner(models.Model):
-    """Extends res.partner to add the customer's national ID (CIN).
-
-    Requirement 2 asks for a 'customer' characterized by
-    name / cin number / phone / email. name, phone and email already
-    exist on res.partner, so only 'cin' needs to be added here.
-    """
     _inherit = 'res.partner'
 
     cin = fields.Char(
